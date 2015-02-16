977017129ab2564915662019c4d2d0dd8f19add4
Andrew <akv9509@saintjoe.edu>
2015-02-16 12:21:05 -0500

    Added the fruit array

---
+++ fruit.js
@@ -0,0 +1,17 @@
+//Andrew Klump
+//CMP237
+//write a array of objects using JSON to store 5 fruits with 2 categories
+//
+//
+
+
+
+{fruits:[
+	{"Name": "Banana", "Seeds": "No"}
+	{"Name": "Apple", "Seeds": "Yes"}
+	{"Name": "Peach", "Seeds": "Yes"}
+	{"Name": "Grape", "Seeds": "No"}
+	{"Name": "Pear", "Seeds": "Yes"}
+
+	]
+}
